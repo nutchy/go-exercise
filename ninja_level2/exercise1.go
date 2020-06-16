57dd833735380df55aef14a6b1b458ee628fc5ff
Chayanon Thongpila <nutchy.th@gmail.com>
2020-06-17 00:30:01 +0700

    Ninja 2: Exercise 1

---
+++ ninja_level2/exercise1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// Write a program that prints a number in decimal, binary, and hex
+func main() {
+	x := 123
+	fmt.Printf("Decimal: %d \n", x)
+	fmt.Printf("Binary: %b \n", x)
+	fmt.Printf("Hex: %#X \n", x)
+}
