f7ccde64d9bb4a3107587872d6ca015772ccc150
Chayanon Thongpila <nutchy.th@gmail.com>
2020-06-17 00:39:14 +0700

    Ninja 2: Exercise 3

--- ninja_level2/exercise1.go
+++ ninja_level2/exercise1.go
@@ -1,4 +1,4 @@
-package exercise_1
+package main
 
 import "fmt"
 
--- ninja_level2/exercise3.go
+++ ninja_level2/exercise3.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// Create type and untype constants, Print the values of the contants.
+const (
+	x        = 1        // untype
+	y string = "Nutchy" // type
+)
+
 // Using the following the operators, write expression and assign their values to variable
 // ==, <= , >=, !=, <, >
 func main() {
@@ -12,4 +18,6 @@ func main() {
 	e := 1 < 1
 	f := 1 > 1
 	fmt.Println(a, b, c, d, e, f)
+	fmt.Println(x)
+	fmt.Println(y)
 }
