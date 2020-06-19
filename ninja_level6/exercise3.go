061f6070d285475e50fc7a48832a7b2e1cbaebf4
Chayanon Thongpila <nutchy.th@gmail.com>
2020-06-20 02:10:24 +0700

    Ninja 6: Exercise 3

--- ninja_level6/exercise3.go
+++ ninja_level6/exercise3.go
@@ -3,11 +3,23 @@ package main
 import "fmt"
 // Use the "defer" keyword to show that a deferred func runs after the func containing it exits
 
-func main() {
-	defer foo()
-	fmt.Println("Main")
-}
 
 func foo() {
 	fmt.Println("Hello from foo")
+}
+
+func bar() {
+	defer func() {
+		fmt.Println("Hello from inner bar")
+	}()
+ 	fmt.Println("Hello from bar")
+}
+
+
+func main() {
+
+	// defer will execute bottom up (Stack)
+	defer foo()
+	defer bar()
+	fmt.Println("Main")
 }
